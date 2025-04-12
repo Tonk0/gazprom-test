2d7874eec5d72fb969748096916d44c0e3242894
Kukyo <thekroyash@gmail.com>
2025-04-12 20:25:19 +0300

    Начальный коммит

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+    'airbnb-typescript',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.app.json',
+    tsconfigRootDir: __dirname
+  },
+  plugins: ['react-refresh'],
+  rules: {
+    'react/react-in-jsx-scope':'off',
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+  },
+}
