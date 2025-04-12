1a696b7565ce26af5c46439efb43529f0baee549
Kukyo <thekroyash@gmail.com>
2025-04-12 21:08:04 +0300

    metrics.sql

--- backend/sql/metrics.sql
+++ backend/sql/metrics.sql
@@ -1,3 +1,14 @@
 SELECT
-  *
-FROM metrics
+  m.id as metric_id,
+  m.datetime as metric_datetime,
+  m.cpu_utilization as metric_cpu_utilization,
+  m.memory_utilization as metric_memory_utilization,
+  m.disk_utilization as metric_disk_utilization,
+  m.node_id as metric_node_id,
+  n.caption as node_caption,
+  n.status as node_status_id,
+  n.interface as node_interface_id,
+  n.admin as node_admin_id
+FROM metrics m
+JOIN
+  nodes n ON m.node_id = n.id
