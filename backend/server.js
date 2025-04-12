8cc3897dcabad5ecf8fc20b15b85b2ad82e564d6
Kukyo <thekroyash@gmail.com>
2025-04-12 22:41:39 +0300

    Добавил cors

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 const server = require("express")();
+const cors = require('cors');
 const port = 23456;
 
 process.env.BASEDIR = process.cwd();
-
+server.use(cors({
+  origin: 'http://localhost:5173'
+}))
 server.use("/api", require("./routes"));
 
 server.listen(port, () => {
